f937f879708682cd495992dc1d16b661403ae8f4
tiagoromano <tiagoromano>
2017-07-12 15:18:24 -0300

    teste

---
+++ src/main/java/blockly/Bloco.java
@@ -0,0 +1,19 @@
+package blockly;
+
+import cronapi.*;
+
+@CronapiMetaData(type = "blockly")
+public class Bloco {
+
+	/**
+	 *
+	 * @return Var
+	 */
+	// Bloco
+	public static Var Executar() throws Exception {
+		Var item = Var.VAR_NULL;
+		item = Var.valueOf("romano teste");
+		return Var.VAR_NULL;
+	}
+
+}
